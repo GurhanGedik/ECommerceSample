fc7c6d7b8ddd79a34948be5f468be90f5143f9c6
Gurhan <gurhangedik@hotmail.com>
2019-09-24 23:27:15 +0300

    Update

---
+++ ECommerce.Sample/ECommerce.Repository/PaymentRepository.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Common;
+using ECommerce.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Repository
+{
+    class PaymentRepository
+    {
+        private static MyECommerceEntities db = Tools.GetConnection();
+
+        public static List<Payment> List()
+        {
+            return db.Payments.ToList();
+        }
+    }
+}
