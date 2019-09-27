0eaa9dafd0e3c0c843c596cd8b7648452ddbf4c1
Gurhan <gurhangedik@hotmail.com>
2019-09-27 21:15:23 +0300

    Update

--- ECommerce.Sample/ECommerce.Repository/PaymentRepository.cs
+++ ECommerce.Sample/ECommerce.Repository/PaymentRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Repository
 {
-    class PaymentRepository
+    public class PaymentRepository
     {
         private static MyECommerceEntities db = Tools.GetConnection();
 
---
+++ ECommerce.Sample/ECommerce.Sample/Controllers/PaymentController.cs
@@ -0,0 +1,45 @@
+﻿using ECommerce.Entity;
+using ECommerce.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.Sample.Controllers
+{
+    public class PaymentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Pay()
+        {
+            ViewBag.PaymentTypes = new SelectList(PaymentRepository.List(), "PaymentId", "PaymentName");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Pay(Invoice model, List<string> PaymentTypes)
+        {
+            model.PaymentDate = DateTime.Now;
+            foreach (string item in PaymentTypes)
+            {
+                int PaymentId = Convert.ToInt32(item);
+                model.PaymentTypeId = PaymentId;
+            }
+            model.OrderId = ((Order)Session["Order"]).OrderId;
+            InvoiceRepository ip = new InvoiceRepository();
+            if (ip.Insert(model).IsSucceeded)
+            {
+                Order ord = (Order)Session["Order"];
+                OrderRepository ordrep = new OrderRepository();
+                ord.IsPay = true;
+                ordrep.Update(ord);
+                Session.Abandon();
+                return RedirectToAction("ListAllProduct", "Home");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
